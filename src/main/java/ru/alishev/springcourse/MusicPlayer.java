79a8afbc6dd90d46a7327687a00bfe0b4c18f30d
FkishDaniels <irina2808777@gmail.com>
2023-03-04 23:02:17 +0300

    HomeWork01 List by Beans

--- src/main/java/ru/alishev/springcourse/MusicPlayer.java
+++ src/main/java/ru/alishev/springcourse/MusicPlayer.java
@@ -1,14 +1,44 @@
 package ru.alishev.springcourse;
 
-public class MusicPlayer {
-    private Music music;
+import java.util.List;
 
-    //IoC
-    public MusicPlayer(Music music){
+public class MusicPlayer {
+    private List<Music> music;
+    private String name;
+    private int Volume;
+
+    public void setMusic(List<Music> music) {
         this.music = music;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return Volume;
+    }
+
+    public void setVolume(int volume) {
+        Volume = volume;
+    }
+
+    //IoC
+    public MusicPlayer(List<Music> music){
+        this.music = music;
+    }
+    public MusicPlayer(){}
+
     public void playMusic(){
-        System.out.println("Playing: "+music.getSong());
+        for (Music mu:music
+             ) {
+            System.out.println(mu.getSong());
+        }
+        System.out.println("On player: "+getName());
+        System.out.println("Volume: "+getVolume());
     }
 }
