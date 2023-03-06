916af839245cfe3b6fba286fbb6866785bc9b289
FkishDaniels <irina2808777@gmail.com>
2023-03-06 19:26:56 +0300

    scope prototype and singleton

--- src/main/java/ru/alishev/springcourse/MusicPlayer.java
+++ src/main/java/ru/alishev/springcourse/MusicPlayer.java
@@ -2,13 +2,45 @@ package ru.alishev.springcourse;
 
 public class MusicPlayer {
     private Music music;
+    private int volume;
+    private String name;
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
 
     //IoC
     public MusicPlayer(Music music){
         this.music = music;
     }
+    public MusicPlayer(){}
 
     public void playMusic(){
         System.out.println("Playing: "+music.getSong());
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "MusicPlayer{" +
+                "music=" + music +
+                ", volume=" + volume +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
--- src/main/java/ru/alishev/springcourse/TestSpring.java
+++ src/main/java/ru/alishev/springcourse/TestSpring.java
@@ -6,10 +6,15 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
 
-        musicPlayer.playMusic();
-
+        firstMusicPlayer.setVolume(80);
+        firstMusicPlayer.setMusic(context.getBean("ClassicalMusic",ClassicalMusic.class));
+        boolean comprassion = firstMusicPlayer == secondMusicPlayer;
+        System.out.println(comprassion);
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
         context.close();
     }
 }
